12e20aed494083cc73389928e738a4c9b31e4be0
Young Foo <fuymsn@hotmail.com>
2015-10-11 00:50:13 +0800

    refactory comment
    refactory comment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+//chat list
+//var $ = function(id){return document.getElementById(id) || null;}
+
+var ChatHead = React.createClass({
+
+	render: function(){
+		return <h3>CHAT</h3>;
+	}
+
+});
+
+//chat person count
+var ChatPersonCount = React.createClass({
+
+	render: function(){
+		return <p id='personCount'>Socket在线人数</p>;
+	}
+
+});
+
 var ChatList = React.createClass({
 	
 	render: function(){
@@ -5,37 +25,65 @@ var ChatList = React.createClass({
 			return <li key={index + itemText}>{itemText}</li>;
 		};
 
-		return <ul className='chat-content'>{this.props.items.map(createItem)}</ul>;
+		//调整滚屏
+		//var dChatList = React.findDOMNode(this.refs.chatList);
+		//dChatList.scrollTop = dChatList.scrollHeight;
+
+		return <ul className='chat-content' rel='chatList'>{this.props.items.map(createItem)}</ul>;
 	}
 
 });
 
-var Chat = React.createClass({
+var ChatForm = React.createClass({
 
-	getInitialState: function(){
-		return {items: [], text: ''}
-	},
-
-	onChange: function(e){
-		this.setState({ text: e.target.value });
-	},
-
-	handleSubmit: function(e){
+	commentSubmit: function(e){
+		//阻止页面刷新
 		e.preventDefault();
-		var nextItems = this.state.items.concat([this.state.text]);
-		var nextText = '';
-		this.setState({ items: nextItems, text: nextText });
+		//comment
+		var co = React.findDOMNode(this.refs.commentInput).value.trim();
+
+		if (!co) {
+			return;
+		};
+		//pass to handle
+		this.props.onHandleSubmit({comment: co});
+		//clear input
+		React.findDOMNode(this.refs.commentInput).value = "";
+		return;
 	},
 
 	render: function(){
 		return (
+			<form onSubmit={this.commentSubmit}>
+				<input type='text' ref="commentInput"/>
+				<button type='submit' placeholder='say something'>Submit</button>
+			</form>
+		);
+	}
+
+});
+
+//chat 外皮
+var Chat = React.createClass({
+
+	getInitialState: function(){
+		return {items: [], comment: ''}
+	},
+
+	handleSubmit: function(data){
+		//拼接字符串
+		var nextItems = this.state.items.concat([data.comment]);
+		//清空comment
+		var nextText = '';
+		this.setState({ items: nextItems, comment: nextText });
+	},
+	render: function(){
+		return (
 			<div>
-				<h3>CHAT</h3>
+				<ChatHead />
+				<ChatPersonCount />
 				<ChatList items={this.state.items} />
-				<form onSubmit={this.handleSubmit}>
-					<input onChange={this.onChange} value={this.state.text} />
-					<button>{'Add #' + (this.state.items.length + 1)}</button>
-				</form>
+				<ChatForm onHandleSubmit={this.handleSubmit} />
 			</div>
 		)
 	}
