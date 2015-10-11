31e9a27741efd1feaf98ae2d03f416be2df93b2e
Young Foo <fuymsn@hotmail.com>
2015-10-11 20:33:05 +0800

    change data load
    change data load

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-//chat list
-//var $ = function(id){return document.getElementById(id) || null;}
+var $ = function(id){return document.getElementById(id) || null;}
+
+var wsServer = 'ws://192.168.0.6:8080';
+var ws = new WebSocket(wsServer);
+
+var isConnect = false;
+
+ws.onopen = function (evt) {
+    console.log("ws链接成功");
+    isConnect = true;
+};
+
+ws.onclose = function (evt) {
+    console.log("ws链接关闭");
+};
+
+/**
+ * {type: "text", msg: {}}
+ * {type: "num", msg: {}}
+ */
+
+ws.onerror = function (evt) {
+    console.log('ws链接失败: ' + evt.data);
+};
+
+
+
 
 var ChatHead = React.createClass({
 
@@ -18,20 +43,40 @@ var ChatPersonCount = React.createClass({
 
 });
 
-// var ChatCommentOther = React.createClass({
+var ChatCommentOther = React.createClass({
 
-// });
+	render: function(){
+		return (
+			<div className='chat-comment_other'>
+				<span className='chat-comment_author'></span>
+				<span className='chat-comment_content'></span>
+			</div>
+		);
+	}
 
-// var ChatCommentMine = React.createClass({
+});
 
-// });
+var ChatCommentMine = React.createClass({
+
+	render: function(){
+		return (
+			<div className='chat-comment_mine'>
+				<span>{this.props.comment.text}</span>
+			</div>
+		);
+	}
+
+});
 
 var ChatList = React.createClass({
 	
 	render: function(){
 
-		var commentNode = this.props.items.map(function(comment, i){
-			return <div key={i}>{comment}</div>;
+		var commentNode = this.props.items.map(function(item, i){
+			//return <div key={i}>{comment}</div>;
+			return (
+				<ChatCommentMine comment={item} key={i+item.text}/>
+			);
 		});
 
 		//调整滚屏
@@ -53,13 +98,19 @@ var ChatForm = React.createClass({
 		//阻止页面刷新
 		e.preventDefault();
 		//comment
-		var co = ReactDOM.findDOMNode(this.refs.commentInput).value.trim();
+		var commentContent = ReactDOM.findDOMNode(this.refs.commentInput).value.trim();
 
-		if (!co) {
+		if (!commentContent) {
 			return;
 		};
 		//pass to handle
-		this.props.onHandleSubmit({comment: co});
+		this.props.onHandleSubmit({user: "Young", text: commentContent});
+
+		//ws handle
+		if (isConnect) {
+			ws.send(commentContent);
+		};
+
 		//clear input
 		ReactDOM.findDOMNode(this.refs.commentInput).value = "";
 		return;
@@ -79,17 +130,46 @@ var ChatForm = React.createClass({
 //chat 外皮
 var Chat = React.createClass({
 
+	//load socket
+	loadInfoFromServer: function(){
+		ws.onmessage = function (evt) {
+		    var data = JSON.parse(evt.data);
+		    switch (data.type) {
+		        case 'text':
+		            //addMsg(data.msg);
+		            this.setState({ comment: data.msg });
+		            break;
+		        case 'num' :
+		            //updataUserNum(data.msg);
+		            break;
+		    }
+
+		    console.log('从Socket服务器获取消息: ' + evt.data);
+		};
+	},
+
 	getInitialState: function(){
-		return {items: [], comment: ''}
+		return {
+			items: [],
+			comment: { 
+				user: "",
+				text: ""
+			}
+		}
+	},
+
+	componentDidMount: function(){
+		this.loadInfoFromServer();
 	},
 
 	handleSubmit: function(data){
 		//拼接字符串
-		var nextItems = this.state.items.concat([data.comment]);
+		var nextItems = this.state.items.concat([data]);
 		//清空comment
-		var nextText = '';
-		this.setState({ items: nextItems, comment: nextText });
+		var nextComment = {};
+		this.setState({ items: nextItems, comment: nextComment });
 	},
+
 	render: function(){
 		return (
 			<div>
