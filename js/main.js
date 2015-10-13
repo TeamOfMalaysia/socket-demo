0de53f307a9f2348d47918f8fc8551be195b64ea
Young Foo <fuymsn@hotmail.com>
2015-10-13 20:40:17 +0800

    Merge branch 'master' of https://github.com/TeamOfMalaysia/socket-demo
    Conflicts:
    	js/main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var $ = function(id){return document.getElementById(id) || null;}
 
 var wsServer = 'ws://127.0.0.1:8080';
+
 var ws = new WebSocket(wsServer);
 
 var isConnect = false;
@@ -72,7 +73,7 @@ var ChatList = React.createClass({
 	render: function(){
 
 		var commentNode = this.props.items.map(function(item, i){
-			if (item.user != "Young") {
+			if (item.user != "0") {
 				return (
 					<ChatCommentOther comment={item} key={i+item.text} />
 				)
@@ -85,7 +86,7 @@ var ChatList = React.createClass({
 		});
 
 		//调整滚屏
-		//var dChatList = React.findDOMNode(this.refs.chatList);
+		//var dChatList = this.refs.chatList;
 		//dChatList.scrollTop = dChatList.scrollHeight;
 
 		return (
@@ -103,7 +104,7 @@ var ChatForm = React.createClass({
 		//阻止页面刷新
 		e.preventDefault();
 		//comment
-		var commentContent = ReactDOM.findDOMNode(this.refs.commentInput).value.trim();
+		var commentContent = this.refs.commentInput.value.trim();
 
 		if (!commentContent) {
 			return;
@@ -117,7 +118,7 @@ var ChatForm = React.createClass({
 		};
 
 		//clear input
-		ReactDOM.findDOMNode(this.refs.commentInput).value = "";
+		this.refs.commentInput.value = "";
 		return;
 	},
 
